7654edfe3789af2f0da94e47c90fff26c650e609
Daruny Salazar Cardoza <dasalaza@car3s1.42barcelona.com>
2023-10-04 21:50:21 +0200

    structure

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,19 +6,20 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 00:44:49 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/09/25 01:24:36 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/10/04 21:35:33 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- * funcion para recorrer una linea y mientras no encuentres un \0 no se creara 
- * un malloc de ese size() y cada vez que lleges a un \0 habra que imprimir la
- * linea.
+ * leemos de un file descriptor FD
+ * funcion para recorrer una linea y mientras no encuentres un \n no se creara 
+ * un malloc de ese size() 
+ * cada vez que lleges a un \n habra que imprimir la linea.
+ *
  * importante es que read retorna un -1 si hay un error, 
  * si se llega al final de linea retorna un 0 
  * mientras no llegue al final del archivo, regresara los caracteres leidos
- * hasta el \0.
- *  
+ * si hay error return NULl
  *
  */
 char	*get_next_line(int fd)
