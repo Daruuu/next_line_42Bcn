9fb35b1152b6eb5bca76f9d4de74d8e419a0a835
Daruuu <thedaruny16@gmail.com>
2023-11-01 17:09:31 +0100

    upd repo laptop

---
+++ cmake-build-debug/CMakeFiles/next_line.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/next_line.dir/get_next_line.c.o"
+  "CMakeFiles/next_line.dir/get_next_line.c.o.d"
+  "CMakeFiles/next_line.dir/get_next_line_utils.c.o"
+  "CMakeFiles/next_line.dir/get_next_line_utils.c.o.d"
+  "next_line"
+  "next_line.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/next_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
